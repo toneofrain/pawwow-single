46f22adebfd7f7af8358de982d2cc394305c604f
toneofrain <toneofrain@gmail.com>
2023-05-17 17:49:55 +0900

    chore: init spring project (#01)

---
+++ src/test/java/dev/saintho/pawwow/PawwowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saintho.pawwow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawwowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

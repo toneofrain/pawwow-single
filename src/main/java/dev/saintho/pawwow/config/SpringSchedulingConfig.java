6baef71780fe1e61b4950ee2d534f4d32e6b621a
toneofrain <toneofrain@gmail.com>
2023-05-18 01:30:21 +0900

    chore: scheduling configuration (#01)

---
+++ src/main/java/dev/saintho/pawwow/config/SpringSchedulingConfig.java
@@ -0,0 +1,9 @@
+package dev.saintho.pawwow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SpringSchedulingConfig {
+}

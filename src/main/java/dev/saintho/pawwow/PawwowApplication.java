057c0495dcc81aeae40a1020d9aef58371d1e18f
조성호 saintho <saintho95@gmail.com>
2023-05-17 17:52:29 +0900

    chore: init spring project (#01) (#2)

---
+++ src/main/java/dev/saintho/pawwow/PawwowApplication.java
@@ -0,0 +1,13 @@
+package dev.saintho.pawwow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawwowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawwowApplication.class, args);
+    }
+
+}

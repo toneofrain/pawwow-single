ddc18f4d2995e045b445691caba8511413f428b4
toneofrain <toneofrain@gmail.com>
2023-05-18 01:28:52 +0900

    chore: async configuration (#01)

---
+++ src/main/java/dev/saintho/pawwow/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package dev.saintho.pawwow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
